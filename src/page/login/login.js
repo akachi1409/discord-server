26956841c190740040e998712316affdec35253d
trusteddev1409 <trusteddev1325@gmail.com>
2022-03-23 01:20:12 +0900

    fix color

--- src/page/login/login.js
+++ src/page/login/login.js
@@ -21,10 +21,10 @@ function Login() {
               {/* <noscript><img sizes="100vw" srcSet="/_next/image?url=%2Fimages%2Fblocksmith_logo.png&amp;w=640&amp;q=75 640w, /_next/image?url=%2Fimages%2Fblocksmith_logo.png&amp;w=750&amp;q=75 750w, /_next/image?url=%2Fimages%2Fblocksmith_logo.png&amp;w=828&amp;q=75 828w, /_next/image?url=%2Fimages%2Fblocksmith_logo.png&amp;w=1080&amp;q=75 1080w, /_next/image?url=%2Fimages%2Fblocksmith_logo.png&amp;w=1200&amp;q=75 1200w, /_next/image?url=%2Fimages%2Fblocksmith_logo.png&amp;w=1920&amp;q=75 1920w, /_next/image?url=%2Fimages%2Fblocksmith_logo.png&amp;w=2048&amp;q=75 2048w, /_next/image?url=%2Fimages%2Fblocksmith_logo.png&amp;w=3840&amp;q=75 3840w" src="/_next/image?url=%2Fimages%2Fblocksmith_logo.png&amp;w=3840&amp;q=75" decoding="async" data-nimg="fill" style="position:absolute;top:0;left:0;bottom:0;right:0;box-sizing:border-box;padding:0;border:none;margin:auto;display:block;width:0;height:0;min-width:100%;max-width:100%;min-height:100%;max-height:100%" loading="lazy"/></noscript> */}
             </span>
           </div>
-          <div className="flex text-4xl items-center flex-shrink-0 px-4 text-white btn-gradient-1 justify-center">
+          <div className="flex text-4xl items-center flex-shrink-0 px-4 text-black justify-center">
             ENIGMA v1.0
           </div>
-          <div className="flex justify-center text-xs text-white btn-gradient-1">
+          <div className="flex justify-center text-xs text-black">
             By NEARverse Labs
           </div>
         </div>
