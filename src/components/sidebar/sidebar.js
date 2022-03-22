35dd23363e56162e8bf7a1b37f1663b43709010d
trusteddev1409 <trusteddev1325@gmail.com>
2022-03-22 19:03:59 +0900

    change fun

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -43,7 +43,7 @@ function Sidebar() {
                       d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
                     ></path>
                   </svg>
-                  <a onClick={onDashboard()}>Dashboard</a>
+                  <a onClick={()=>onDashboard()}>Dashboard</a>
                 </div>
               ) : (
                 <div className="text-gray-300 hover:bg-gray-700 mt-2 hover:text-white group flex items-center px-2 py-2 mt-2 text-sm font-medium rounded-md">
@@ -62,7 +62,7 @@ function Sidebar() {
                       d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
                     ></path>
                   </svg>
-                  <a onClick={onDashboard()}>Dashboard</a>
+                  <a onClick={()=>onDashboard()}>Dashboard</a>
                 </div>
               )}
               {localStorage.getItem("sidebar") === "account" ? (
@@ -82,7 +82,7 @@ function Sidebar() {
                       d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
                     ></path>
                   </svg>
-                  <a onClick={onAccount()}>Account</a>
+                  <a onClick={()=>onAccount()}>Account</a>
                 </div>
               ) : (
                 <div className="text-gray-300 hover:bg-gray-700 mt-2 hover:text-white group flex items-center px-2 py-2 mt-2 text-sm font-medium rounded-md">
@@ -101,7 +101,7 @@ function Sidebar() {
                       d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
                     ></path>
                   </svg>
-                  <a onClick={onAccount()}>Account</a>
+                  <a onClick={()=>onAccount()}>Account</a>
                 </div>
               )}
             </nav>
