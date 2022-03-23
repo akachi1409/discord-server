22fa6eb5427e64005bbe0436653b2846e5ff6383
trusteddev1409 <trusteddev1325@gmail.com>
2022-03-24 00:41:06 +0900

    fix design layout

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -20,23 +20,24 @@ function Sidebar() {
     <div>
       <div className="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
         <div className="flex-1 flex flex-col min-h-0 bg-black text-black dark:bg-gray-800 dark:text-white">
-          <div className="flex-1 flex flex-row pt-5 pb-4 overflow-y-auto">
-            <img
-              srcSet={Logo}
-              src={Logo}
-              decoding="async"
-              data-nimg="fill"
-              className="logo-img"
-            />
-            <div className="flex-1 flex flex-col">
-              <div className="flex-1 justify-center text-lg text-white">
-                ENIGMA v1.0
-              </div>
-              <div className="flex-1 justify-center text-xs text-white">
-                By NEARverse Labs
+          <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
+            <div className="flex-1 flex flex-row">
+              <img
+                srcSet={Logo}
+                src={Logo}
+                decoding="async"
+                data-nimg="fill"
+                className="logo-img"
+              />
+              <div className="flex-1 flex flex-col">
+                <div className="flex-1 justify-center text-lg text-white">
+                  ENIGMA v1.0
+                </div>
+                <div className="flex-1 justify-center text-xs text-white">
+                  By NEARverse Labs
+                </div>
               </div>
             </div>
-
             <nav className="mt-10 flex-1 px-2 space-y-1">
               {localStorage.getItem("sidebar") === "dashboard-mine" ||
               localStorage.getItem("sidebar") === "dashboard-other" ? (
