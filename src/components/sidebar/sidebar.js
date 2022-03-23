885c330db7b380f4d7098ebbb3bcd97733de301d
trusteddev1409 <trusteddev1325@gmail.com>
2022-03-24 00:58:45 +0900

    fix font

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -21,7 +21,7 @@ function Sidebar() {
       <div className="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
         <div className="flex-1 flex flex-col min-h-0 bg-black text-black dark:bg-gray-800 dark:text-white">
           <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
-            <div className="flex-1 flex flex-row">
+            <div className="flex flex-row">
               <img
                 srcSet={Logo}
                 src={Logo}
